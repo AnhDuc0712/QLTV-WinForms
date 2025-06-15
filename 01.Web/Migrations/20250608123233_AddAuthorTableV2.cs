e75f31a7bb07e542f5304997937e5b36a38814df
MSI\admin <phuoctri204@gmail.com>
2025-06-15 17:31:40 +0700

    capnhat

---
+++ 01.Web/Migrations/20250608123233_AddAuthorTableV2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ngducanh.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAuthorTableV2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
